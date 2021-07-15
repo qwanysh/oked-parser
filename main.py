012a4130a90ec2439ea9946b28b405b472c0a85c
qwanysh <olimpsdu@gmail.com>
2021-07-15 17:38:32 +0600

    Add meta column

--- main.py
+++ main.py
@@ -16,39 +16,50 @@ def get_oked_by_bin(bin):
 
         if response.status_code == httpx.codes.OK:
             response_json = response.json()
-            return response_json['obj']['okedCode']
+            if response_json['success']:
+                return response_json['obj']['okedCode']
         elif response.status_code == httpx.codes.BAD_REQUEST:
             break
 
         time.sleep(0.2)
 
 
-def get_bin_column_index(worksheet):
+def get_column_index_by_value(worksheet, value):
     for index, cell in enumerate(next(worksheet.rows)):
-        if cell.value == 'bin':
+        if cell.value == value:
             return index
+    return -1
 
 
 def main():
     workbook = openpyxl.load_workbook('companies.xlsx')
     worksheet = workbook.active
-    bin_column_index = get_bin_column_index(worksheet)
-    oked_column = worksheet.max_column + 1
+    bin_column_index = get_column_index_by_value(worksheet, value='bin')
 
-    # inserting oked column header
-    worksheet.cell(row=1, column=oked_column).value = 'oked'
+    oked_column_index = get_column_index_by_value(worksheet, value='oked')
+    if oked_column_index == -1:
+        oked_column_index = worksheet.max_column
+        worksheet.cell(row=1, column=oked_column_index + 1).value = 'oked'
+
+    meta_column_index = get_column_index_by_value(worksheet, value='meta')
+    if meta_column_index == -1:
+        meta_column_index = oked_column_index + 1
+        worksheet.cell(row=1, column=meta_column_index + 1).value = 'meta'
+
+    workbook.save('companies.xlsx')
 
     for row, row_cells in enumerate(worksheet.rows, start=1):
-        # skipping header row
-        if row == 1:
+        if row == 1 or row_cells[meta_column_index].value:
             continue
 
         bin = row_cells[bin_column_index].value
         oked = get_oked_by_bin(bin)
-        worksheet.cell(row=row, column=oked_column).value = oked or '-'
-
-    workbook.save('companies_with_oked.xlsx')
+        worksheet.cell(row=row, column=oked_column_index + 1).value = oked or '-'
+        worksheet.cell(row=row, column=meta_column_index + 1).value = 'parsed'
+        workbook.save('companies.xlsx')
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print('total time:', time.time() - start_time)
